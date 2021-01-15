9514307b2726bf5ad240ff2aa25c6e8f63f73a29
qiqzhao <qiqzhao@thoughtworks.com>
2021-01-15 11:11:52 +0800

    feat: import postcss

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,37 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 let config = {
-  entry: './src/index.jsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx$/,
-        exclude: /node_module/,
-        use: 'babel-loader'
-      },
-      {
-        test: /\.(le|c)ss$/,
-        use: [
-          {loader: 'style-loader'},
-          {loader: "css-loader"},
-          {loader: "less-loader"}
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ]
+	entry: './src/index.jsx',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx$/,
+				exclude: /node_module/,
+				use: 'babel-loader'
+			},
+			{
+				test: /\.(le|c)ss$/,
+				use: [
+					{ loader: 'style-loader' },
+					{ loader: "css-loader" },
+					{ loader: "less-loader" },
+					{
+						loader: "postcss-loader",
+						options: { sourceMap: true }
+					},
+				]
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html'
+		})
+	]
 }
 
 module.exports = config;
