6448b3a51005c4de3928612ed0481677047cc27a
qiqzhao <qiqzhao@thoughtworks.com>
2021-01-28 16:05:56 +0800

    refactor: check unused dep

--- __test__/Home.test.tsx
+++ __test__/Home.test.tsx
@@ -4,7 +4,6 @@ import Home from '../src/pages/home/Home';
 
 describe('Home component', () => {
   it('renderer', () => {
-    expect(2).toEqual(1 + 1);
     const wrapper = shallow(<Home />);
     expect(wrapper).toMatchSnapshot();
   });
